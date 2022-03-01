c2bcc67274804a6a07ada9b0621e2ccde026ff82
LasseDall <lass794rrstud.kea.dk>
2022-03-01 09:08:36 +0100

    udskrivMenu tilføjet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     char[] alfabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
 
+    public void udskrivMenu(){
+        System.out.printf("1) Cæsar %n2) Vigenére %n0) Exit");
+    }
+
     public int bogstavTilTal(char bogstav){
         int index = -1;
         for (int i=0; i< alfabet.length; i++){
