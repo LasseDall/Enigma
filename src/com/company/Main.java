8104c55d222737ac0edb6bea23d1f08bf2e6f14a
LasseDall <lass794rrstud.kea.dk>
2022-03-01 08:36:00 +0100

    talTilBogstav og bogstavTilTal tilføjet

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    char[] alfabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
+
+    public int bogstavTilTal(char bogstav){
+        int index = -1;
+        for (int i=0; i< alfabet.length; i++){
+            if(bogstav ==alfabet[i]){
+                index = i;
+                i = alfabet.length;
+            }
+        }
+        return index;
+    }
+
+    public char talTilBogstav(int tal){
+        char bogstav = alfabet[tal];
+        return bogstav;
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
